9ad564c0dcbea7b9892d861b8ed9efbfb6890f3f
Maksim Levin <delinrus@gmail.com>
2025-04-12 10:48:09 +0300

    Fixed Test

--- src/test/java/ru/job4j/devops/controllers/CalcControllerTest.java
+++ src/test/java/ru/job4j/devops/controllers/CalcControllerTest.java
@@ -65,8 +65,8 @@ class CalcControllerTest {
 
     @Test
     public void whenNegativeNumber2() {
-        var input = new TwoArgs(-5, -5);
-        var expected = new Result(-10);
+        var input = new TwoArgs(-6, -6);
+        var expected = new Result(-12);
         var output = new CalcController().summarise(input);
         assertThat(output.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
         assertThat(output.getBody()).isEqualTo(expected);
